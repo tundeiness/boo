e9561b44eed98be6409b32524fc095048d638bd3
tundeiness <tundeiness@yahoo.co.uk>
2022-09-03 12:06:04 +0100

    remove unwanted files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Nothing here!!
         <p>
           Edit
           {' '}
