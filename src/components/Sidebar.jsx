5d6c37e58763b2b9dc25dd00f2ea296032d90579
tundeiness <tundeiness@yahoo.co.uk>
2022-09-03 23:54:36 +0100

    wip filter bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+// import { HiOutlineSquares2X2 } from 'react-icons/hi';
+import { IoGridOutline } from 'react-icons/io5';
 
 const Sidebar = () => {
   const test = 0;
   return (
-    <aside className="w-96 border border-blue-400">Sidebar</aside>
+    <aside className="w-96 border border-blue-400">
+      <p className="text-lg font-semibold leading-9">Soulverse</p>
+      <p className="text-xs mb-6">MBTI personality database</p>
+
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[150px] mx-16 border border-red-500">
+        <IoGridOutline size={14} />
+        <input
+          className="bg-transparent py-0.5 px-2 w-full focus:outline-none"
+          type="text"
+          placeholder="categories"
+        />
+      </div>
+    </aside>
   );
 };
 
