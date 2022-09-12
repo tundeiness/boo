8953216ed5bf92d72899f31024b123f2fa828270
tundeiness <tundeiness@yahoo.co.uk>
2022-09-12 18:51:44 +0100

    large screen styling

--- src/components/SubCards.jsx
+++ src/components/SubCards.jsx
@@ -7,8 +7,10 @@ const SubCards = ({ title, text }) => {
   return (
     <>
       <div className="w-23 h-23 xl:w-40 xl:h-40 lg:w-32 lg:h-36 rounded-xl shadow-lg drop-shadow-lg py-4 flex flex-col justify-between items-center align-middle border border-gray">
-        <p className="lg:w-[60%] w-full mb-5 text-center">{title}</p>
-        <p className="lg:text-2xl text-md text-viking font-semibold text-center">{text}</p>
+        <p className="xl:w-[60%] lg:w-[70%] w-full mb-5 text-center">{title}</p>
+        <p className="xl:text-2xl lg:text-xl text-md text-viking font-semibold text-center">
+          {text}
+        </p>
       </div>
     </>
   );
